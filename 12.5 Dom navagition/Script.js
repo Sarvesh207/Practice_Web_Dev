e7c1e7c3c29ec98de9a24ffe4e6c8916b2540a6e
Sarvesh207 <sarveshgaynar@gmail.com>
2023-01-14 08:53:20 +0530

    did some parctice

---
+++ 12.5 Dom navagition/Script.js
@@ -0,0 +1,15 @@
+// const countingUL = document.getElementById("counting")
+
+// const ulChildren = countingUL.children
+
+// const ulParent = countingUL.parentNode;
+
+// console.log(ulChildren);
+// console.log(ulParent);
+
+
+const oneLi = document.getElementById("one");
+
+// const siblingOneLi = oneLi.parentNode.childNodes;
+
+console.log(oneLi.firstChild.nodeValue);
